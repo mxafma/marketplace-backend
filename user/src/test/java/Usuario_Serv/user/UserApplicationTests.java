f7514f77ff10912950311a753247be219504664f
Hardalvss <alvarobaros29@gmail.com>
2025-05-22 10:47:12 -0400

    Add files via upload
    Microservicio Gestion De Usuario

---
+++ user/src/test/java/Usuario_Serv/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package Usuario_Serv.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

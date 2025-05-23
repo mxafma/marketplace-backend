e4d2887acab37f3f7fa8858fa260a87b8bdd624c
Matias Arauz <ma.arauz@duocuc.cl>
2025-05-22 23:35:48 -0400

    Otra correcion nombre (quite el _microservice)

---
+++ moderation/src/main/java/com/moderation/cl/moderation/ModerationApplication.java
@@ -0,0 +1,13 @@
+package com.moderation.cl.moderation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModerationApplication.class, args);
+	}
+
+}
---
+++ moderation/src/test/java/com/moderation/cl/moderation/ModerationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moderation.cl.moderation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModerationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

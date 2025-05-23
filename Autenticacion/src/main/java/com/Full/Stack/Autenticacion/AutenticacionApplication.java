b31f754191f35f342f9ae0555113a17eed1b6669
MarioGrateronDuoc <ma.grateron@duocuc.cl>
2025-05-22 21:38:31 -0400

    autenticacion

---
+++ Autenticacion/src/main/java/com/Full/Stack/Autenticacion/AutenticacionApplication.java
@@ -0,0 +1,19 @@
+package com.Full.Stack.Autenticacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@SpringBootApplication
+public class AutenticacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutenticacionApplication.class, args);
+	}
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}

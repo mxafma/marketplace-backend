2c63905a863eb9cebb25af54775bae504fb7e0ea
MarioGrateronDuoc <ma.grateron@duocuc.cl>
2025-05-23 10:08:28 -0400

    auth

--- compra/src/main/java/com/marketplace/compra/Repository/CompraRepository.java
+++
@@ -1,7 +0,0 @@
-package com.marketplace.compra.Repository;
-
-import com.marketplace.compra.Model.Compra;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CompraRepository extends JpaRepository<Compra, Long> {
-}
